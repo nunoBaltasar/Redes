21c07a64490ed8c5d65ac89139caccbef9ba5e49
joao vitor lobo <jovitorlobo@gmail.com>
2026-09-23 11:27:51 +0100

    fix: melhorias

--- user.cpp
+++ user.cpp
@@ -11,7 +11,7 @@
 #include <netdb.h>
 #include <string.h>
 #include <csignal>
-
+#include <cerrno>
 
 using namespace std;
 
@@ -110,189 +110,181 @@ void checkResponseLogin(char* buffer, User& user){
         cout << "new user registered\n";
         user.setLIN();
     }else if (!strcmp(buffer, "ERR\n")){
-        cout << "Error: idk\n";
+        cout << "protocol error\n";
+    }else{
+        cout << "unexpected reply: " << buffer;
     }
 }
+
 void checkResponseUNR(char* buffer, User& user){
     if (!strcmp(buffer, "RUR OK\n")){
         cout << "successful unregister\n";
         user.setLOUT();
     }else if (!strcmp(buffer, "RUR NOK\n")){
-        cout << "unknown user\n";
-    }else if (!strcmp(buffer, "RUR WSP\n")){
-        cout << "incorrect unregister attempt\n";
+        cout << "unknown user or not logged in\n";
+    }else if (!strcmp(buffer, "RUR WRP\n")){  // fixed: was "WSP"
+        cout << "incorrect password\n";
+    }else if (!strcmp(buffer, "RUR UNR\n")){  // added missing case
+        cout << "user not registered\n";
     }else if (!strcmp(buffer, "ERR\n")){
-        cout << "Error:\n";
+        cout << "protocol error\n";
+    }else{
+        cout << "unexpected reply: " << buffer;
     }
 }
-void checkRespondeLOUT(char* buffer, User& user){  
+
+void checkRespondeLOUT(char* buffer, User& user){
     if (!strcmp(buffer, "RLO OK\n")){
         cout << "successful logout\n";
         user.setLOUT();
     }else if (!strcmp(buffer, "RLO NLG\n")){
         cout << "user not logged in\n";
-    }else if (!strcmp(buffer, "RLO WRP\n")){
+    }else if (!strcmp(buffer, "RLO WRP\n")){ 
+        cout << "incorrect password\n";
+    }else if (!strcmp(buffer, "RLO UNR\n")){ 
         cout << "unknown user\n";
+    }else if (!strcmp(buffer, "ERR\n")){
+        cout << "protocol error\n";
+    }else{
+        cout << "unexpected reply: " << buffer;
     }
 }
 
-void sendAndReceive(char *buffer, User& user, string request, int fd, addrinfo* res, sockaddr_in addr){
-    int n;
-    socklen_t addrlen;
-    n = sendto(fd, request.c_str(), request.length(), 0, res->ai_addr, res->ai_addrlen);
-    if (n == -1) exit(1);
-    addrlen = sizeof(addr);
-    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
-    if (n >= 0) {buffer[n] = '\0';}
-}
-
-//é preciso tratar do sigpipe
-int main(int argc, char *argv[]){
-    string peerport;
-    string dsip;
-    string dsport;
-    int i = 1, isDSPORT = 0, isDSIP = 0;
-     
-    while (i < argc){
-        string arg = argv[i];
-        if (arg == "-m"){
-            i++;
-            peerport = argv[i];
-        }else if (arg == "-n"){
-            i++;
-            dsip = argv[i];
-            isDSIP = 1;
-        }else if (arg == "-p"){
-            i++;
-            dsport = argv[i];
-            isDSPORT = 1;
-        }
-        i++;
-    }
-    if (!isDSPORT){
-        dsport = "59000";
-    }
-    if (!isDSIP){
-        dsip = "193.136.138.142"; //192.168.1.1
-    }
-    int fd, errcode;
+int sendAndReceive(char *buffer, string request, int fd, addrinfo* res, sockaddr_in addr){
     ssize_t n;
     socklen_t addrlen;
-    struct sockaddr_in addr;
-    struct addrinfo hints, *res;
-    struct timeval tmout;
-    tmout.tv_sec = 6.7;
-    tmout.tv_usec = 0;
-    fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (fd == -1) {
-        cout << "ERR: ";
-        exit(1);
-    };
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype =  SOCK_DGRAM;
-    errcode = getaddrinfo(dsip.c_str(), dsport.c_str(), &hints, &res);
-    if (errcode != 0){ 
-        cout << "ERR: fah\n";
-        cout << errcode;
-        exit(1);
-    };
-    string input;
-    string command;
-    User user{};
-    char buffer[128];
+    n = sendto(fd, request.c_str(), request.length(), 0, res->ai_addr, res->ai_addrlen);
+    if (n == -1){
+        if (errno != EINTR) perror("sendto");
+        return -1;
+    }
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, 128, 0, (struct sockaddr*) &addr, &addrlen);
+    if (n == -1){
+        if (errno != EINTR) perror("recvfrom");
+        return -1;
+    }
+    buffer[n] = '\0';
+    return 0;
+}
+
+int main(int argc, char *argv[]){
+    string peerport;
+    string dsip   = "193.136.138.142";
+    string dsport = "59000";
+    bool hasPeerport = false;
+
+    // SIGINT (Ctrl+C): sets stopped=1; no SA_RESTART so getline is
+    // interrupted immediately without needing to press Enter
     struct sigaction signalAction{};
     sigemptyset(&signalAction.sa_mask);
     signalAction.sa_handler = signalHandler;
     signalAction.sa_flags = 0;
     sigaction(SIGINT, &signalAction, nullptr);
-    
-    if (setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &tmout, sizeof(tmout)) < 0){
-        cout << "Error: socket timeout";
-        freeaddrinfo(res);
-        close(fd);
-        return 0;
+
+    // SIGPIPE: ignore — prevents crash if network connection breaks
+    signal(SIGPIPE, SIG_IGN);
+
+    // parse ./user -m peerport [-n DSIP] [-p DSport]
+    for (int i = 1; i < argc; i++){
+        string arg = argv[i];
+        if (arg == "-m"){
+            if (i + 1 >= argc){ cout << "Missing value for -m\n"; exit(1); }
+            peerport = argv[++i];
+            hasPeerport = true;
+        }else if (arg == "-n"){
+            if (i + 1 >= argc){ cout << "Missing value for -n\n"; exit(1); }
+            dsip = argv[++i];
+        }else if (arg == "-p"){
+            if (i + 1 >= argc){ cout << "Missing value for -p\n"; exit(1); }
+            dsport = argv[++i];
+        }else{
+            cout << "Unknown argument: " << arg << "\n";
+            cout << "Usage: " << argv[0] << " -m peerport [-n DSIP] [-p DSport]\n";
+            exit(1);
+        }
     }
 
-    while (getline(cin, input)){
-        stringstream ss(input);
-        ss >> command;
-        string word;
-        
-        if (command == "login"){
-            if (!checkLogin(input, user)) continue;
-            
-            string request = "LIN " + user.getUID() + " " + user.getPW() + " " + peerport + '\n';
-            
-            sendAndReceive(buffer, user, request, fd, res, addr);
+    if (!hasPeerport){
+        cout << "Usage: " << argv[0] << " -m peerport [-n DSIP] [-p DSport]\n";
+        exit(1);
+    }
 
-            checkResponseLogin(buffer, user);
-             
-        }else if (command == "unregister"){
-            //nao sei como verificar reply nok e wrp
-            if (ss >> word){
-                cout << "Too many arguments\n";
+    int fd, errcode;
+    struct sockaddr_in addr;
+    struct addrinfo hints, *res;
+    struct timeval tmout;
+    tmout.tv_sec  = 6;  
+    tmout.tv_usec = 0;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1){ perror("socket"); exit(1); }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    errcode = getaddrinfo(dsip.c_str(), dsport.c_str(), &hints, &res);
+    if (errcode != 0){
+        cout << "getaddrinfo: " << gai_strerror(errcode) << "\n";
+        close(fd);
+        exit(1);
+    }
+
+    if (setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, &tmout, sizeof(tmout)) < 0){
+        cout << "Error: socket timeout\n";
+        freeaddrinfo(res);
+        close(fd);
+        return 1;
+    }
+
+    string input, command;
+    User user{};
+    char buffer[128];
+
+    while (!stopped && getline(cin, input)){
+        stringstream ss(input);
+        if (!(ss >> command)) continue; // blank line
+        string word;
+
+        if (command == "login"){
+            if (user.getLIN()){
+                cout << "Already logged in as " << user.getUID() << "\n";
                 continue;
             }
-            if (!user.getLIN()){
-                cout << "user not logged in\n";
-                continue;
-            }       
-            string request = "UNR " + user.getUID() +" " + user.getPW() + '\n';
+            if (!checkLogin(input, user)) continue;
 
-            sendAndReceive(buffer, user, request, fd, res, addr);
+            string request = "LIN " + user.getUID() + " " + user.getPW() + " " + peerport + '\n';
+            if (sendAndReceive(buffer, request, fd, res, addr) == -1) continue;
+            checkResponseLogin(buffer, user);
 
+        }else if (command == "unregister"){
+            if (ss >> word){ cout << "Too many arguments\n"; continue; }
+            if (!user.getLIN()){ cout << "No user is currently logged in\n"; continue; }
+
+            string request = "UNR " + user.getUID() + " " + user.getPW() + '\n';
+            if (sendAndReceive(buffer, request, fd, res, addr) == -1) continue;
             checkResponseUNR(buffer, user);
 
         }else if (command == "logout"){
-            if (ss >> word){
-                cout << "Too many arguments\n";
-                continue;
-            }
-            if (!user.getLIN()){
-                cout << "user not logged in\n";
-                continue;
-            }
-            string request = "LOU " + user.getUID() +" " + user.getPW() + '\n';
-            
-            sendAndReceive(buffer, user, request, fd, res, addr);
+            if (ss >> word){ cout << "Too many arguments\n"; continue; }
+            if (!user.getLIN()){ cout << "No user is currently logged in\n"; continue; }
 
+            string request = "LOU " + user.getUID() + " " + user.getPW() + '\n';
+            if (sendAndReceive(buffer, request, fd, res, addr) == -1) continue;
             checkRespondeLOUT(buffer, user);
 
-
-        }else if(command == "publish filename label"){
-            string fn = "fillename";
-            if (FILE *file = fopen(fn.c_str(), "r")) {
-                fclose(file);
-            } else {
-                cout << "There is no file to send in the current directory\n";
-                continue;
-            }
-            continue;
-            
-        }else if (command == "list"){ //nao sei qual o tamanho do buffer caso receba mt nomes
-            string request = "LST\n";
-            sendAndReceive(buffer, user, request, fd, res, addr);
-
-            if (!strcmp(buffer, "RLS OK"))
-            continue;
         }else if (command == "exit"){
-            if (ss >> word){
-                cout << "Too many arguments\n";
-            }
-            if (user.getLIN()){
-                cout << "It is advised to logout before closing the program\n";
-                continue;
-            }
+            if (ss >> word){ cout << "Too many arguments\n"; continue; }
+            if (user.getLIN()){ cout << "Please logout before exiting\n"; continue; }
             break;
+
         }else{
             cout << "Invalid command\n";
         }
-
     }
 
     if (stopped){
-        cout << "Program exiting safely\n";
+        cout << "\nProgram exiting safely\n";
     }
 
     freeaddrinfo(res);
